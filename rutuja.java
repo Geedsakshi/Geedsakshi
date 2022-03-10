635f6fe08f5300e2b4916deff823e066bef2306e
Geedsakshi <101328125+Geedsakshi@users.noreply.github.com>
2022-03-10 17:18:23 +0530

    Add files via upload

---
+++ rutuja.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public clas Main
+{
+public static void main(String args[i])
+{
+int count=0;
+int i=0;
+String s;
+ArrayList<String>list=new ArrayList<String>();
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter Input;");
+while(true){
+s=sc.nextLine();
+if(s.equals("Stop"))break;
+list.add(s);
+}
+System.out.println(Entered Input is:");
+for(String str.list){
+System.out.println(str);
+}
+}
+}
